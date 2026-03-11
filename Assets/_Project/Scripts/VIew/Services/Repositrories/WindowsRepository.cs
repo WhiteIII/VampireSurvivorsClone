c4ca21bc7749ed493c5d9e854f1b8cdd6c699748
alexandrKozhanyazov <whiteminacraft90@gmail.com>
2026-03-12 00:58:48 +0500

    added ui controller base logic

--- Assets/_Project/Scripts/VIew/Services/Repositrories/WindowsRepository.cs
+++ Assets/_Project/Scripts/VIew/Services/Repositrories/WindowsRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using _Project.Scripts.Common.Repositories.Base;
 using _Project.Scripts.View.Base;
+using Cysharp.Threading.Tasks;
 using Object = UnityEngine.Object;
 
 namespace _Project.Scripts.View.Services.Repositrories
@@ -11,6 +12,21 @@ namespace _Project.Scripts.View.Services.Repositrories
     {
         private readonly List<Window> _windows = new();
 
+        public bool TryGet<T>(out T resultWindow)
+            where T : Window
+        {
+            resultWindow = null;
+            foreach (Window window in _windows)
+            {
+                if (window is T concreteWindow)
+                {
+                    resultWindow = concreteWindow;
+                    return true;
+                }
+            }
+            return false;
+        }
+        
         public T Add<T>(T window)
             where T : Window
         {
@@ -18,17 +34,39 @@ namespace _Project.Scripts.View.Services.Repositrories
             return window;
         }
 
+        public async UniTask CloseAndDestroyWindows(params Type[] windowTypes)
+        {
+            if (windowTypes.Length == 0)
+                return;
+            int windowTypesIndex = 0;
+            Queue<Window> windowsToRemove = new(windowTypes.Length);
+            foreach (Window window in _windows)
+            {
+                if (window.GetType() == windowTypes[windowTypesIndex])
+                {
+                    windowTypesIndex++;
+                    await window.CloseAsync();
+                    windowsToRemove.Enqueue(window);
+                    Object.Destroy(window);
+                    if (windowTypesIndex == windowTypes.Length)
+                        break;
+                }
+            }
+            while (windowsToRemove.Count > 0)
+                _windows.Remove(windowsToRemove.Dequeue());
+        }
+        
         public void DisableInteractableAllWindows() => 
             InvokeAllWindowsMethods(window => window.DisableInteractable());
         
         public void EnableInteractableAllWindows() => 
             InvokeAllWindowsMethods(window => window.EnableInteractable());
         
-        public void DisableInteractableAllWindowsWithout(params Type[] types) => 
-            InvokeIfWindowTypeNotIn(types, window => window.DisableInteractable());
+        //public void DisableInteractableAllWindowsWithout(params Type[] types) => 
+          //  InvokeWindowMethodIf(types, window => window.DisableInteractable());
         
-        public void EnableInteractableAllWindowsWithout(params Type[] types) => 
-            InvokeIfWindowTypeNotIn(types, window => window.EnableInteractable());
+        //public void EnableInteractableAllWindowsWithout(params Type[] types) => 
+          //  InvokeWindowMethodIf(types, window => window.EnableInteractable(), );
 
         public void DestroyAllWindows()
         {
@@ -40,11 +78,11 @@ namespace _Project.Scripts.View.Services.Repositrories
         private void InvokeAllWindowsMethods(Action<Window> action) => 
             _windows.ForEach(action);
         
-        private void InvokeIfWindowTypeNotIn(Type[] types, Action<Window> action)
+        private void InvokeWindowMethodIf(Type[] types, Action<Window> action, Predicate<Window> predicate)
         {
             _windows.ForEach(window => types.ToList().ForEach(type =>
             {
-                if (window.GetType() != type)
+                if (predicate(window))
                     action(window);
             }));
         }
--- Assets/_Project/Scripts/VIew/Services/UIController.cs
+++ Assets/_Project/Scripts/VIew/Services/UIController.cs
@@ -1,13 +1,48 @@
+using System;
 using _Project.Scripts.View.Base;
 using _Project.Scripts.View.Services.Repositrories;
+using Cysharp.Threading.Tasks;
 using Zenject;
 
 namespace _Project.Scripts.View.Services
 {
     public class UIController 
     {
-        private readonly IFactory<IWindow> _windowFactory;
+        private readonly IFactory<Window>[] _windowFactories;
         private readonly WindowsRepository _repository;
 
+        public UIController(IFactory<Window>[] windowFactories, WindowsRepository repository)
+        {
+            _windowFactories = windowFactories;
+            _repository = repository;
+        }
+
+        public async UniTask CreateAndOpenWindowAsync<T>()
+            where T : Window
+        {
+            if (_repository.TryGet(out T window))
+            {
+                await window.OpenAsync();
+                return;
+            }
+            await GetWindowFactory<T>().Create().OpenAsync();
+        }
+
+        public async UniTask DestroyAndCloseWindowsAsync(params Type[] windows)
+        {
+            
+            await _repository.CloseAndDestroyWindows(windows);
+        } 
+        
+        private IFactory<T> GetWindowFactory<T>()
+            where T : Window
+        {
+            foreach (IFactory<Window> windowFactory in _windowFactories)
+            {
+                if (windowFactory is IFactory<T> resultFactory)
+                    return resultFactory;
+            }
+            return null;
+        }
     }
 }
