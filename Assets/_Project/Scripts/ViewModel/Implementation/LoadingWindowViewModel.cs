e62f52a03d4ba8a16c52742572b740c24cbd8d5f
alexandrKozhanyazov <whiteminacraft90@gmail.com>
2026-06-30 18:06:57 +0500

    update loading window logic

--- Assets/_Project/Scripts/ViewModel/Implementation/LoadingWindowViewModel.cs
+++ Assets/_Project/Scripts/ViewModel/Implementation/LoadingWindowViewModel.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using _Project.Scripts.ViewModel.Base;
 using Cysharp.Threading.Tasks;
@@ -13,6 +14,9 @@ namespace _Project.Scripts.ViewModel.Implementation
 
         public async UniTask StartLoadingAsync(params UniTask[] tasks)
         {
+            if (MultiStageLoadingInProgress)
+                throw new Exception("Multi stage loading in progress!");
+            
             List<UniTask> tasksList = new(tasks);
             int tasksCount = tasksList.Count;
             float succeededTasksCount = 0;
@@ -52,18 +56,75 @@ namespace _Project.Scripts.ViewModel.Implementation
                 return false;
             return true;
         }
-        
-        public void ResetLoadingProgress() => 
+
+        public void ResetLoadingProgress()
+        {
             _progress.Value = 0;
+            _multiStageLoading = null;
+        } 
     }
 
     public partial class LoadingWindowViewModel
     {
-        private int _totalCount;
-        
-        public void SetTotalCountTasks(int totalCount) => 
-            _totalCount = totalCount;
-        
-        
+        private MultiStageLoading _multiStageLoading;
+
+        public bool MultiStageLoadingInProgress
+        {
+            get
+            {
+                if (_multiStageLoading == null)
+                    return false;
+                return _multiStageLoading.InProgress;
+            }
+        }
+
+        private void StartMultiStageLoading(int totalTasksCount) => 
+            _multiStageLoading = new MultiStageLoading(totalTasksCount);
+
+        public async UniTask WaitLoadingForMultiStageLoadingAsync(params UniTask[] tasks)
+        {
+            if (MultiStageLoadingInProgress == false)
+                throw new Exception("Multi stage loading in not progress!");
+
+            _progress.Value = await _multiStageLoading.WaitLoadingAndCheckMultistageLoadingStatusAsync(tasks);
+        }
+
+        private class MultiStageLoading
+        {
+            private readonly int _totalCount;
+            private int _currentCount;
+            
+            public bool InProgress { get; private set; } = true;
+            
+            public MultiStageLoading(int totalCount) => 
+                _totalCount = totalCount;
+            
+            public async UniTask<float> WaitLoadingAndCheckMultistageLoadingStatusAsync(params UniTask[] tasks)
+            {
+                if (InProgress == false)
+                    InProgress = true;
+                _currentCount += tasks.Length;
+                if (_currentCount > _totalCount)
+                    throw new Exception("Too many tasks!");
+                
+                while (CheckTasksComplete(tasks) == false)
+                    await UniTask.Yield();
+                
+                if (_currentCount == _totalCount)
+                    InProgress = false;
+                
+                return (float)_currentCount / _totalCount;
+            }
+
+            private bool CheckTasksComplete(UniTask[] tasks)
+            {
+                foreach (UniTask task in tasks)
+                {
+                    if (task.Status == UniTaskStatus.Pending)
+                        return false;
+                }
+                return true;
+            }
+        }
     }
 }
